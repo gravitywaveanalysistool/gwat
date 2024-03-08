e16631c8e8140251046eedceee5bdc2659dadbb2
piesarentsquare <jjhh12@outlook.com>
2024-03-07 20:08:24 -0500

    started build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+# BUILD_OS=$(uname)
+# if [[ ${BUILD_OS} == *"MSYS"* ]] || [[ ${BUILD_OS} == *"MINGW"* ]]; then
+#     BUILD_OS="Windows"
+# elif [[ ${BUILD_OS} == "Darwin" ]]; then
+#     BUILD_OS="macOS"
+#     Platform=${Platform:-$(arch)}
+# fi
+
+# if [ ${BUILD_OS} == "Windows" ]; then
+
+# elif [ ${BUILD_OS} == "Linux" ]; then
+
+#pretend ubuntu is the only os
+sudo apt-get install curl cmake g++ python3-venv
+cd lib/gdl
+./scripts/build_gdl.sh prep
+./scripts/build_gdl.sh configure
+./scripts/build_gdl.sh build
+
+mkdir gdl_build
+INSTALL_PREFIX=gdl_build & ./scripts/build_gdl.sh install 
+python3 -m venv pyenv

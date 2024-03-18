8c083cf036a8a3029e6ce8abfdcab7fd0bc3bef5
piesarentsquare <jjhh12@outlook.com>
2024-03-18 12:40:26 -0400

    added curl to dependencies

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
-BUILD_OS=$(uname)
-if [[ ${BUILD_OS} == *"MSYS"* ]] || [[ ${BUILD_OS} == *"MINGW"* ]]; then
-    BUILD_OS="Windows"
-elif [[ ${BUILD_OS} == "Darwin" ]]; then
-    BUILD_OS="macOS"
-    Platform=${Platform:-$(arch)}
-fi
+# BUILD_OS=$(uname)
+# if [[ ${BUILD_OS} == *"MSYS"* ]] || [[ ${BUILD_OS} == *"MINGW"* ]]; then
+#     BUILD_OS="Windows"
+# elif [[ ${BUILD_OS} == "Darwin" ]]; then
+#     BUILD_OS="macOS"
+#     Platform=${Platform:-$(arch)}
+# fi
 
-if [ ${BUILD_OS} == "Windows" ]; then
+# if [ ${BUILD_OS} == "Windows" ]; then
 
-elif [ ${BUILD_OS} == "Linux" ]; then
+# elif [ ${BUILD_OS} == "Linux" ]; then
 
-    # if command -v apt-get >/dev/null 2>&1; then
-    #     sudo apt-get install curl cmake g++ python3-venv -y
-    # else
-    #     log "Fatal error: unsupported package manager."
-    #     exit 1
-    #     # yum and zypper package install for curl cmake g++ python3-venv
-    # fi
+#     # if command -v apt-get >/dev/null 2>&1; then
+#     #     sudo apt-get install curl cmake g++ python3-venv -y
+#     # else
+#     #     log "Fatal error: unsupported package manager."
+#     #     exit 1
+#     #     # yum and zypper package install for curl cmake g++ python3-venv
+#     # fi
 
-else
-    log "Fatal error: unknown operating system."
-    exit 1
-fi
+# else
+#     log "Fatal error: unknown operating system."
+#     exit 1
+# fi
 
 cd lib/gdl
 ./scripts/build_gdl.sh install_toolchain

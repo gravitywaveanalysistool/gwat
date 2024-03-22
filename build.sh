b4b236b58613a7d32206f35741ceb9dcde7e101d
piesarentsquare <jjhh12@outlook.com>
2024-03-22 00:27:44 -0400

    add python-gdl to site-packages

--- build.sh
+++ build.sh
@@ -1,25 +1,28 @@
-function install_package {
-    if [ $(uname) == "Linux" ]; then
-        if command -v apt >/dev/null 2>&1; then
-            sudo apt update
-            sudo apt install $@ -y
-        elif command -v yum >/dev/null 2>&1; then
-            sudo yum install $@ -y
-        elif command -v zypper >/dev/null 2>&1; then
-            sudo zypper install $@ -y
-        else
-            echo "Fatal error: unsupported package manager."
-            exit 1
-        fi
+if [ $(uname) == "Linux" ]; then
+    if command -v apt >/dev/null 2>&1; then
+        sudo apt update
+        sudo apt install python3 python3 venv python3-gdl -y
+        python3 -m venv pyenv
+        cp `dpkg -L python3-gdl | grep .so` `find pyenv -type d -name site-packages`
+
+    elif command -v yum >/dev/null 2>&1; then
+        sudo yum install python3 python3 venv python3-gdl -y
+        python3 -m venv pyenv
+        
+
+    elif command -v zypper >/dev/null 2>&1; then
+        sudo zypper install python3 python3 venv python3-gdl -y
+        python3 -m venv pyenv
+        
     else
-        echo "Fatal error: unsupported operating system."
+        echo "Fatal error: unsupported package manager."
         exit 1
     fi
+else
+    echo "Fatal error: unsupported operating system."
+    exit 1
+fi
 
-}
-
-install_package python3 python3-venv
-python3 -m venv pyenv
 source ./pyenv/bin/activate
 pip install numpy matplotlib tk
 install_package python3-gdl

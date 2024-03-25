8e4ce6aa2f8dbd5e226fdbfb38614c429659677c
piesarentsquare <jjhh12@outlook.com>
2024-03-25 12:59:52 -0400

    added zlib to deps

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 if [ $(uname) == "Linux" ]; then
     if command -v apt >/dev/null 2>&1; then
         sudo apt update
-        sudo apt install python3 python3 python3-venv python-gdl -y
+        sudo apt install python3 python3 python3-venv python-gdl zlib1g-dev -y
         python3 -m venv pyenv
         cp `dpkg -L python-gdl | grep .so` `find pyenv -type d -name site-packages`
 
     elif command -v yum >/dev/null 2>&1; then
-        sudo yum install python3 python3 python3-venv python3-gdl -y
+        sudo yum install python3 python3 python3-venv python3-gdl zlib-devel -y
         python3 -m venv pyenv
         
 
     elif command -v zypper >/dev/null 2>&1; then
-        sudo zypper install python3 python3 python3-venv python3-gdl -y
+        sudo zypper install python3 python3 python3-venv python3-gdl zlib-devel -y
         python3 -m venv pyenv
         
     else

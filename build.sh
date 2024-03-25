a6f93b15aeedbf28ea514d8b331b3d52add6ef2d
piesarentsquare <jjhh12@outlook.com>
2024-03-25 12:47:55 -0400

    fixed python-gdl name

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 if [ $(uname) == "Linux" ]; then
     if command -v apt >/dev/null 2>&1; then
         sudo apt update
-        sudo apt install python3 python3 python3-venv python3-gdl -y
+        sudo apt install python3 python3 python3-venv python-gdl -y
         python3 -m venv pyenv
-        cp `dpkg -L python3-gdl | grep .so` `find pyenv -type d -name site-packages`
+        cp `dpkg -L python-gdl | grep .so` `find pyenv -type d -name site-packages`
 
     elif command -v yum >/dev/null 2>&1; then
         sudo yum install python3 python3 python3-venv python3-gdl -y

92e683d147a38b9deca2e316efe67c3a29856587
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 22:22:07 -0400

    Added a clunky but functional toolbar for panning the graph.

--- src/ui/graphframe.py
+++ src/ui/graphframe.py
@@ -1,4 +1,5 @@
 import customtkinter
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
@@ -9,8 +10,15 @@ class GraphFrame(customtkinter.CTkFrame):
         self.canvas = customtkinter.CTkCanvas(self)
         self.canvas.grid(row=0, column=0, padx=15, pady=15)
 
+        self.toolbar_frame = customtkinter.CTkFrame(self)
+        self.toolbar_frame.grid(row=1, column=0, padx=15, pady=15)
+
     def draw_plot(self, fig):
         self.canvas.delete('all')
         self.canvas.fig_agg = FigureCanvasTkAgg(fig, master=self.canvas)
         self.canvas.fig_agg.draw()
         self.canvas.fig_agg.get_tk_widget().pack(fill=customtkinter.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(self.canvas.fig_agg, self.toolbar_frame)
+        toolbar.update()
+        toolbar.pack(side=customtkinter.BOTTOM, fill=customtkinter.X)

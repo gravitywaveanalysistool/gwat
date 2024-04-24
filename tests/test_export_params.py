818990710528a96dd0763c584301826064f4c450
piesarentsquare <jjhh12@outlook.com>
2024-04-23 23:39:36 -0400

    fix export params tests

--- tests/test_export_params.py
+++ tests/test_export_params.py
@@ -1,99 +1,82 @@
-from src.utils import save_params_to_file
+from src.utils import save_params_to_file, get_temp_folder
 import unittest
 import os
 
-def generateBadTropo():
-    tropoParams = {"lol" : "ANOTHER STRING?", "pizza" : 112, "borb" : "hehe"}
-    return tropoParams
 
-def generateBadStrato():
-    stratoParams = {"bad" : "good?", "good" : "good?"}
+def generate_tropo():
+    tropo_params = {"Horizontal Wavelength": "32.45", "Vertical Wavelength": "1.987",
+                   "Mean Phase Propogation Direction": "284.34",
+                   "Upward Propogation Fraction": "56.95",
+                   "Zonal Momentum Flux": "-0.135", "Meridional Momentum Flux": "0.0345", "Potential Energy": "27.12",
+                   "Kinetic Energy": "5.21"}
+    return tropo_params
 
-def generateGoodTropo():
-    tropoParams = {"Horizontal Wavelength": 32.45, "Vertical Wavelength": 1.987,
-                   "Mean Phase Propogation Direction": 284.34,
-                   "Upward Propogation Fraction": 56.95,
-                   "Zonal Momentum Flux": -0.135, "Meridional Momentum Flux": 0.0345, "Potential Energy": 27.12,
-                   "Kinetic Energy": 5.21}
-    return tropoParams
 
-def generateGoodStrato():
-    stratoParams = {"Horizontal Wavelength": 400.51, "Vertical Wavelength": 1.375,
+def generate_strato():
+    strato_params = {"Horizontal Wavelength": 400.51, "Vertical Wavelength": 1.375,
                     "Mean Phase Propogation Direction": 263.92,
                     "Upward Propogation Fraction": 81.03,
                     "Zonal Momentum Flux": -0.004, "Meridional Momentum Flux": -0.0005, "Potential Energy": 1.54,
                     "Kinetic Energy": 4.26}
-    return stratoParams
+    return strato_params
+
+
+def cleanup(file_path):
+    if os.path.isfile(file_path):
+        os.remove(file_path)
 
 
 class TestSaveParamsToFile(unittest.TestCase):
 
-    def test_export_params_to_file(self, stratoParams, tropoParams, filePath):
+    def test_good_path(self):
+        """
+        Test to ensure a correctly formatted file will be generated
+        """
 
-        def cleanup():
-            if os.path.exists(filePath):
-                os.remove(filePath)
+        strato_params, tropo_params = generate_strato(), generate_tropo()
 
-        def contains(currentLayer, key, val):
-            if currentLayer == "Strato:":
-                if key in stratoParams.keys and val == stratoParams[key]:
-                    return True
-            elif currentLayer == "Tropo:":
-                if key in tropoParams.keys and val == tropoParams[key]:
+        file_path = get_temp_folder() + "params.txt"
+
+        self.addCleanup(lambda: cleanup(file_path))
+
+        save_params_to_file(strato_params, tropo_params, file_path)
+
+        # Checks if the file exists
+        self.assertTrue(os.path.exists(file_path))
+
+        def contains_line(params):
+            for i, (key, val) in enumerate(params.items()):
+                if (key + " " + str(val)).split() == line.split():
                     return True
             return False
 
-
-        save_params_to_file(stratoParams, tropoParams, filePath)
-
-        self.addCleanup(cleanup)
-
-        # Checks if the file exists
-        self.assertTrue(os.path.exists(filePath))
-
         # Validates the contents of the file
-        f = open(filePath, "r")
-        currentLayer = "none"
+        f = open(file_path, "r")
+        current_layer = "none"
         for line in f:
-            if line.strip() == "Strato:" or line.strip() == "Tropo:":
-                currentLayer = line.strip()
+            if line.strip() == "Stratosphere:" or line.strip() == "Troposphere:":
+                current_layer = line.strip()
             else:
-                key, val = line.split()
-                self.assertTrue(contains(currentLayer, key, val))
+                # ensure there is content in the line
+                if line.strip():
+                    # ensure the content of the line is found in the correct dictionary
+                    self.assertTrue(contains_line(strato_params if current_layer == "Stratosphere:" else tropo_params))
 
         f.close()
 
-
-    def test_goodStrato_goodTropo(self):
-        """
-        Test that a file which contains formatted tropo and strato parameters will be generated on good data input
-        """
-        tropoParams, stratoParams = generateGoodTropo(), generateGoodStrato()
-        self.test_export_params_to_file(stratoParams,tropoParams, filePath="params.txt")
-
-    def test_goodStrato_badTropo(self):
-        """
-        Test that the method will catch when it has invalid arras passed to it and stop execution before creating a bad parameters file
-        """
-        stratoParams = generateGoodStrato()
-        tropoParams = generateBadTropo()
-        self.test_export_params_to_file(stratoParams,tropoParams, filePath="params.txt")
-
-    def test_badStrato_goodTropo(self):
-        """
-        Same as previous test, opposite order
-        """
-        stratoParams = generateBadStrato()
-        tropoParams = generateGoodTropo()
-        self.test_export_params_to_file(stratoParams,tropoParams, filePath="params.txt")
-
-    def test_badFilepath(self):
+    def test_bad_path(self):
         """
         Make sure that if the output filepath does not exist/is not valid to cancel execution and alert the user
         """
-        stratoParams, tropoParams = generateGoodStrato(), generateGoodTropo()
-        self.test_export_params_to_file(stratoParams,tropoParams, filePath="params.txt")
+        strato_params, tropo_params = generate_strato(), generate_tropo()
+
+        file_path = ".."
+
+        self.addCleanup(lambda: cleanup(file_path))
+
+        self.assertRaises(PermissionError, lambda: save_params_to_file(strato_params, tropo_params, file_path))
 
 
 if __name__ == '__main__':
     unittest.main()
+

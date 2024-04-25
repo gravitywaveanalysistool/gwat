336382ad7ac9bc18df865ea45075276c1d221613
Gooseclemons <scottwilmot04@gmail.com>
2024-04-25 00:55:08 -0400

    Finished tests for save_graphs_as_pngs

--- tests/test_export_graphs_as_png.py
+++ tests/test_export_graphs_as_png.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from src.utils import save_graphs_as_png
+from src.utils import save_graphs_as_png, get_temp_folder
 from src.graphing.xygraph import XYGraph
 import pandas as pd
 
@@ -28,30 +28,40 @@ def gen_graphs(count):
 
 # save_graphs_as_png(graph_objects, folder_path, selected_graphs)
 class TestExportGraphsAsPng(unittest.TestCase):
-
-    def export_test(self, num_graphs, selected_graphs):
+    def export_test(self, num_graphs, selected_graphs, expected_pngs):
+        directoryPath = get_temp_folder()
+        graphFolderPath = directoryPath + "graphs"
+        save_graphs_as_png(gen_graphs(num_graphs), directoryPath, selected_graphs)
 
         def cleanup():
-            if os.path.isdir('tests'):
-                for filename in os.listdir('tests'):
-                    os.remove(filename)
-                os.rmdir('tests')
+            if os.path.isdir(graphFolderPath):
+                for filename in os.listdir(graphFolderPath):
+                    os.remove(graphFolderPath + "/" + filename)
+                os.rmdir(graphFolderPath)
 
         self.addCleanup(cleanup)
 
-        save_graphs_as_png()
+        file_should_exist = expected_pngs > 0
+        self.assertEqual(os.path.isdir(graphFolderPath), file_should_exist)
+
+        if file_should_exist:
+            graph_pngs = os.listdir(graphFolderPath)
+            self.assertEqual(expected_pngs, len(graph_pngs))
 
     def test_no_graphs(self):
-        pass
+        self.export_test(0, [], 0)
 
     def test_no_selected_graphs(self):
-        pass
+        self.export_test(2, [], 0)
+
+    def test_no_graphs_1_selected_graph(self):
+        self.export_test(0, ['0'], 0)
 
     def test_key_mismatch(self):
-        pass
+        self.export_test(4, ['wacky key'], 0)
 
     def test_export_single_graph(self):
-        pass
+        self.export_test(1, ['0'], 2)
 
     def test_multiple_graphs(self):
-        pass
+        self.export_test(4, ['0','1','2'], 6)
